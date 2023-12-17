6fdea03b39224a03708bf20979e047fdc72ffb1c
nopeless <38830903+nopeless@users.noreply.github.com>
2023-12-17 17:24:08 -0500

    command for building image files from drawio diagrams

---
+++ genfiles.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\draw.io\draw.io.exe" --crop -x -o "Plastic straws.png" "Plastic straws.drawio" 
+"C:\Program Files\draw.io\draw.io.exe" --crop -x -o "sentimental analysis pipeline.png" "sentimental analysis pipeline.drawio" 
